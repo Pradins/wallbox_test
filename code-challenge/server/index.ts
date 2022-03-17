bcefa681a5d9563298470afaa63c4d22d018ac44
Albert Prats <pradins@gmail.com>
2022-03-17 11:48:03 +0800

    feat(addServer): add logic to forward messages from charger to widget

--- code-challenge/server/index.ts
+++ code-challenge/server/index.ts
@@ -1,42 +1,96 @@
-import WebSocket from "ws";
+import WebSocket from 'ws';
+
+enum CharginStatus {
+    charging = 'charging',
+    charging80 = 'charging80',
+    charged = 'charged'
+};
 
 const chargersWidgetMap = new Map<string, { widget: string }>();
-chargersWidgetMap.set("c0001", { widget: "w0001" });
-chargersWidgetMap.set("c0002", { widget: "w0002" });
-chargersWidgetMap.set("c1234", { widget: "wABCD" });
+chargersWidgetMap.set('c0001', {widget: 'w0001'});
+chargersWidgetMap.set('c0002', {widget: 'w0002'});
+chargersWidgetMap.set('c1234', {widget: 'wABCD'});
 
 const widgetWebSocketsMap = new Map<string, WebSocket>();
 
+function getDeviceId(url: string) {
+    // TODO: can this be done in a better way ? more fancy
+    const splitUrl = url && url.split('/');
+    return splitUrl && splitUrl[splitUrl.length - 1];
+}
+
+function getChargingStatus(soc: number) {
+    if (soc === 100) {
+        return CharginStatus.charged;
+    } else if (soc >= 80) {
+        return CharginStatus.charging80;
+    }
+    return CharginStatus.charging;
+}
+
 function createChargersServer(port = 3100): WebSocket.Server {
-  const wss = new WebSocket.Server({ port });
+    const wss = new WebSocket.Server({port});
 
-  wss.on("connection", (ws, req) => {
-    console.log("Charger connection attempt to: ", req.url);
-    ws.on("message", (message) => {
-      console.log(message.toString());
+    wss.on('connection', (ws, req) => {
+        console.log('Charger connection attempt to: ', req.url);
+
+        const chargerId = req.url && getDeviceId(req.url);
+
+        ws.on('message', (message) => {
+            let chargingValue: number | undefined;
+
+            if (typeof message === 'string') {
+                chargingValue = JSON.parse(message).data.soc;
+            }
+
+            const widgetID = chargerId && chargersWidgetMap.get(chargerId)?.widget;
+
+            // TODO: improve this typing
+            const widgetWebsocket: "" | undefined | WebSocket = widgetID && widgetWebSocketsMap.get(widgetID);
+
+            if (chargingValue) {
+                const messageToSend = {
+                    event: 'chargingStatus',
+                    data: {
+                        status: getChargingStatus(chargingValue),
+                    }
+                }
+
+                widgetWebsocket && widgetWebsocket.send(JSON.stringify(messageToSend), {}, (err: Error) => {
+                    if (err) {
+                        console.log('error sending message to widget: ', widgetID);
+                    } else {
+                        console.log('message delivered successfully from server to widget: ', widgetID);
+                    }
+                });
+            }
+        });
+
+        ws.on('close', () => {
+            console.log('charger connection closed');
+        });
     });
 
-    ws.on("close", () => {
-      console.log("charger connection closed");
-    });
-  });
-
-  return wss;
+    return wss;
 }
 
 function createWidgetsServer(port = 3200): WebSocket.Server {
-  const wss = new WebSocket.Server({ port });
+    const wss = new WebSocket.Server({port});
 
-  wss.on("connection", (ws, req) => {
-    console.log("Widget connection attempt to: ", req.url);
-    ws.on("close", () => {
-      console.log("widget connection closed");
+    wss.on('connection', (ws, req) => {
+        console.log('Widget connection attempt to: ', req.url);
+
+        const widgetId = req.url && getDeviceId(req.url);
+        widgetId && widgetWebSocketsMap.set(widgetId, ws);
+
+        ws.on('close', () => {
+            console.log('widget connection closed');
+        });
     });
-  });
 
-  return wss;
+    return wss;
 }
 
 createChargersServer();
 createWidgetsServer();
-console.log("Servers ON");
+console.log('Servers ON');
